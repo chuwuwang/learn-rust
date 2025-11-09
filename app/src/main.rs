ed61eb5489658f7c00e31fb4e87fc311a5528c8a
chuwuwang <chuwuwang@126.com>
2025-11-09 21:34:02 +0800

    feat: add tracing

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,9 +6,12 @@ mod book_pal;
 mod communicator;
 
 use std::future;
+use tracing::info;
+use tracing_subscriber::{fmt, prelude::*};
 use tokio::runtime;
 use tokio::runtime::Runtime;
-use crate::book_pal::{file_test, reqwest_test, tokio_test };
+
+use crate::book_pal::{file_test, reqwest_test, tokio_test};
 use crate::book_pal::process_test;
 use crate::book_pal::arc_test;
 use crate::book_pal::channel_test;
@@ -16,6 +19,17 @@ use crate::book_pal::condvar_test;
 use crate::book_pal::{ rayon_test };
 
 fn main() {
+    let timme = fmt::time::LocalTime::rfc_3339();
+    let subscriber = fmt::Subscriber::builder()
+        .with_timer(timme) // 本地时间格式
+        .with_level(true) // 显示日志级别
+        .with_span_events(fmt::format::FmtSpan::ACTIVE) // 显示跨度激活/结束
+        .finish();
+    // 设置全局订阅者
+    tracing::subscriber::set_global_default(subscriber).expect("初始化订阅者失败");
+    // 后续的追踪操作会通过该订阅者处理
+    info!("程序启动");
+
     // communicator::client::connect();
     // connector::client::connect();
     let result = Runtime::new().unwrap().block_on( connector::test_server() );
